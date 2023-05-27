cc06bd3fb95b4df3824c49d26028d9f247e1838f
Goncharov <GVVic@mail.ru>
2023-05-27 20:56:41 +0700

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+P/S Не стал ограничивать проверку 5ю числами
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int i = 0;
+while (i < (number.Length/2))
+{
+if(Convert.ToInt32(number[i].ToString()) == Convert.ToInt32(number[number.Length-(1+i)].ToString()))
+   {
+    i++;
+    }
+    else
+    goto Negative;
+}
+Console.WriteLine("Число является полиндромом");
+return; // делал через break; но почему то через break у меня не сработало. Задать вопрос?
+
+Negative:
+Console.WriteLine("Число не является палиндромом");
